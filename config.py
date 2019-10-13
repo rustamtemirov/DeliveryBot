5ea484080c3782c3467d52fd131ae8303e2a32cb
Russel331 <rustikjon99@gmail.com>
2019-10-13 18:27:52 +0500

    added id

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 token = '940947743:AAGW8D_j42q6dImzqURHfcKiJaS_yQ1BYSw'
 db_name = 'database.db'
-admin_id = [657845923, 89848329, 132634425, 740824495, 36950100]
+admin_id = [36950100, 89848329, 132634425, 740824495, ]
 channel_id = -1001434042703
