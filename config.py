5fb0b0ae502918763010b41ce942b84aec504316
Russel331 <rustikjon99@gmail.com>
2019-10-25 15:21:42 +0500

    changed tok

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '779126181:AAFCTQjzU2GykDA35apRi3i9nNlOvtK52kE'
+token = '822899939:AAG16UIewdfG3kIiZKoo20H9BfscWkw0CZc'
 db_name = 'database.db'
 admin_id = [36950100, 865951058, 132634425, 740824495, 657845923, ]
 channel_id = -1001434042703
