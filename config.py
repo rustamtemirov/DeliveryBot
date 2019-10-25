a181e57da0e20c0caaf5f12dd7c2789a727d83dd
Russel331 <rustikjon99@gmail.com>
2019-10-25 08:39:01 +0500

    changed token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '940947743:AAGW8D_j42q6dImzqURHfcKiJaS_yQ1BYSw'
+token = '779126181:AAFCTQjzU2GykDA35apRi3i9nNlOvtK52kE'
 db_name = 'database.db'
 admin_id = [36950100, 865951058, 132634425, 740824495, 657845923, ]
 channel_id = -1001434042703
